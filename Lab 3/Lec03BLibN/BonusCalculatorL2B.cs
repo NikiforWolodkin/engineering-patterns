41443ea4364cb7dad9d73c22583d2162c127e5af
User <mrdoggydog37@gmail.com>
2023-03-09 21:48:52 +0300

    Finished project 3

--- Lab 3/Lec03BLibN/BonusCalculatorL2B.cs
+++ Lab 3/Lec03BLibN/BonusCalculatorL2B.cs
@@ -6,24 +6,22 @@ using System.Threading.Tasks;
 
 namespace Lec03BLibN
 {
-    internal class BonusCalculatorL3B : IBonus
+    internal class BonusCalculatorL2B : IBonus
     {
         public float CostOneHour { get; set; }
         public float A { get; set; }
-        public float B { get; set; }
         public float X { get; set; }
 
-        public BonusCalculatorL3B(float costOneHour, float a, float b, float x)
+        public BonusCalculatorL2B(float costOneHour, float a, float x)
         {
             CostOneHour = costOneHour;
             A = a;
-            B = b;
             X = x;
         }
 
         public float Calculate(float numberOfHours)
         {
-            return (numberOfHours + A) * (CostOneHour + B) * X;
+            return (numberOfHours + A) * CostOneHour * X;
         }
     }
 }
--- Lab 3/Lec03BLibN/BonusCalculatorL3B.cs
+++ Lab 3/Lec03BLibN/BonusCalculatorL3B.cs
@@ -6,22 +6,24 @@ using System.Threading.Tasks;
 
 namespace Lec03BLibN
 {
-    internal class BonusCalculatorL2B : IBonus
+    internal class BonusCalculatorL3B : IBonus
     {
         public float CostOneHour { get; set; }
         public float A { get; set; }
+        public float B { get; set; }
         public float X { get; set; }
 
-        public BonusCalculatorL2B(float costOneHour, float a, float x)
+        public BonusCalculatorL3B(float costOneHour, float a, float b, float x)
         {
             CostOneHour = costOneHour;
             A = a;
+            B = b;
             X = x;
         }
 
         public float Calculate(float numberOfHours)
         {
-            return (numberOfHours + A) * CostOneHour * X;
+            return (numberOfHours + A) * (CostOneHour + B) * X;
         }
     }
 }
